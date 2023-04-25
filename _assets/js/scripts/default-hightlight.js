d781899d4f8f85d421fe04b77a9c0eb236a3bff5
David Colebatch <david@tidalmigrations.com>
2023-04-24 20:26:50 -0400

    remove tidalsw.com site, redirect to tidalcloud.com

--- _assets/js/scripts/default-hightlight.js
+++
@@ -1,9 +0,0 @@
-$('.pricing-table__header').hover( function() {
-  $('.pricing-table__header.active-tb').removeClass('selected');
-});
-
-$('.pricing-table__header').mouseleave(function(){
-  $('.pricing-table__header.active-tb').addClass('selected');
-});
-
-
